Fixed computation time and modifed latencies

PPO.py
@@ -7,6 +7,7 @@ import preprocessing_representations
 from torch_geometric.nn import global_mean_pool
 from torch_geometric.data import Data, Batch
 from torch.nn.utils.rnn import pad_sequence, pack_padded_sequence
+import statistics
 
 
 
@@ -53,19 +54,29 @@ class RolloutBuffer:
         del self.critic_states_req[:]
 
 class ActorCriticWorker(nn.Module):
-    def __init__(self, num_nodes, num_agents, action_dims, inference=False):
+    def __init__(self, num_nodes, num_agents, action_dims, inference=False, messages=False, modified=False):
         super(ActorCriticWorker, self).__init__()
         self.zeros_arr = torch.zeros(action_dims, dtype=torch.float32)
         self.action_dims = action_dims
         self.inference = inference
         self.num_agents = num_agents
+        self.messages = messages
+        self.modified = modified
         
         application_gnn_input_dim = 3 # CPU, Data, is_current
-        application_embedding_dim = 3
+
+        if messages:
+            application_embedding_dim = 3 + 2 # plus messages
+        else: 
+            application_embedding_dim = 3
 
         self.application_gnn = GNNApplication(input_dim=application_gnn_input_dim, hidden_dim_gnn=16, output_dim=application_embedding_dim) # 3 node features CPU, Data, is_current
 
         input_dim_gnn_infr = 4 # CPU, Cost, Coeff, has_predecessor, has_latest_request
+
+        if modified:
+            input_dim_gnn_infr = 5
+
         self.policy_gnn = GNNAttention(input_dim=input_dim_gnn_infr, hidden_dim_gnn=64, embedding_dim=application_embedding_dim+1, attention_dim=32, output_dim=1, num_nodes=num_nodes)
 
         self.critic_gnn = GNNCritic(input_dim=input_dim_gnn_infr, hidden_dim_gnn=64, embedding_dim=application_embedding_dim+1, attention_dim=32, output_dim=1, num_nodes=num_nodes)
@@ -85,10 +96,14 @@ class ActorCriticWorker(nn.Module):
             if state['request_dependencies'][i][1] == 4:
                 predecessors.append(int(state['request_dependencies'][i][0]))
 
-        # For the current period we need the minimum available capacities
-        cpu = [int(min(node_capacity)) for node_capacity in state['node_capacities']]
+        avg_cpu = [statistics.mean(node_capacity) for node_capacity in state['node_capacities']]
+        cpu = [int(min(node_capacity)) for node_capacity in state['node_capacities']] # min cpu
 
-        graph_repr = preprocessing_representations.produce_infr_repr(cpu, state['node_costs'], state['device_coef'], state['latencies'], state['current_allocation'], predecessors)
+        if self.modified:
+            graph_repr = preprocessing_representations.produce_infr_repr_mod(avg_cpu, cpu, state['node_costs'], state['device_coef'], state['latencies'], state['current_allocation'], predecessors)
+        else:
+            graph_repr = preprocessing_representations.produce_infr_repr(cpu, state['node_costs'], state['device_coef'], state['latencies'], state['current_allocation'], predecessors)
+
         ### Tensorize Inputs
         dependencies_repr = Batch.from_data_list([dependencies_repr])
         graph_repr_batch = Batch.from_data_list([graph_repr])
@@ -96,18 +111,19 @@ class ActorCriticWorker(nn.Module):
 
         ### Actor Model
         req_embeddings = self.application_gnn(dependencies_repr).squeeze() # process current request with attention to future requests
-        # print(state["requests_left"])
-        # print("req_embeddings", req_embeddings.shape)
 
         requests_left = torch.tensor([state['requests_left']], dtype=torch.float32)
-        # print("requests_left", requests_left.shape)
-        req_embeddings = torch.cat((req_embeddings, requests_left), dim=0) # Concatenate along the feature dimension
-        
+
+        if self.messages:
+            other_agents_messages = torch.tensor([state['others_requests_left'], state['others_utilization']], dtype=torch.float32)
+            req_embeddings = torch.cat((req_embeddings, requests_left, other_agents_messages), dim=0) # Concatenate along the feature dimension
+        else:
+            req_embeddings = torch.cat((req_embeddings, requests_left), dim=0)
+
         if not batch:
             req_embeddings = req_embeddings.unsqueeze(0)
         node_scores = self.policy_gnn(graph_repr_batch, req_embeddings).squeeze() # process graph with attention to requests
 
-        # print("node_scores", node_scores)
         action_probs = torch.softmax(node_scores, dim=0) # Shape: (num_of_nodes,)
     
         if action_mask is not None:
@@ -158,14 +174,14 @@ class ActorCriticWorker(nn.Module):
     
 
 class PPO_MARL:
-    def __init__(self, nodes_per_agent, shared_nodes, action_dims, lr_gnn, gamma, K_epochs, eps_clip, number_of_agents, inference=False):
+    def __init__(self, nodes_per_agent, shared_nodes, action_dims, lr_gnn, gamma, K_epochs, eps_clip, number_of_agents, inference=False, messages=False, modified=False):
         self.nodes_per_agent = nodes_per_agent
         self.action_dims = action_dims
         self.gamma = gamma
         self.eps_clip = eps_clip
         self.K_epochs = K_epochs
-        self.policy = ActorCriticWorker(nodes_per_agent+shared_nodes, number_of_agents, action_dims, inference).to(device)
-        self.policy_old = ActorCriticWorker(nodes_per_agent+shared_nodes, number_of_agents, action_dims, inference).to(device)
+        self.policy = ActorCriticWorker(nodes_per_agent+shared_nodes, number_of_agents, action_dims, inference, messages, modified).to(device)
+        self.policy_old = ActorCriticWorker(nodes_per_agent+shared_nodes, number_of_agents, action_dims, inference, messages, modified).to(device)
         self.buffer = RolloutBuffer()
         self.num_agents = number_of_agents
         self.buffers = [RolloutBuffer() for _ in range(number_of_agents)]

preprocessing_representations.py
@@ -124,7 +124,8 @@ def produce_local_app_repr(task_features, dependencies, current_ms, num_microser
 
     return data
 
-def produce_infr_repr(node_capacities, node_costs, device_coef, latencies, current_app_allocation, predecessors):
+
+def produce_infr_repr(min_capacities, node_costs, device_coef, latencies, current_app_allocation, predecessors):
     num_nodes = latencies.shape[0]
     pred_allocated_indicator = np.zeros(num_nodes, dtype=np.float32)
     # has_last_request = np.zeros(num_nodes, dtype=np.float32)
@@ -132,7 +133,32 @@ def produce_infr_repr(node_capacities, node_costs, device_coef, latencies, curre
         assigned_node = current_app_allocation[task_idx]
         if assigned_node >= 0:  # Check if it has been allocated
             pred_allocated_indicator[assigned_node] = 1.0
-    node_features = np.stack([node_capacities, node_costs, device_coef, pred_allocated_indicator], axis=1)
+    node_features = np.stack([min_capacities, node_costs, device_coef, pred_allocated_indicator], axis=1)
+    node_features = torch.tensor(node_features, dtype=torch.float32)
+
+    edge_index = []
+    edge_attr = []
+    for i in range(num_nodes):
+        for j in range(num_nodes):
+            if latencies[i, j] > 0:
+                edge_index.append([i, j])
+                edge_attr.append([latencies[i, j]])
+
+    edge_index = torch.tensor(edge_index, dtype=torch.long).t()  # shape [2, num_edges]
+    edge_attr = torch.tensor(edge_attr, dtype=torch.float32)
+
+    data = Data(x=node_features, edge_index=edge_index, edge_attr=edge_attr)
+    return data
+
+def produce_infr_repr_mod(avg_capacities ,min_capacities, node_costs, device_coef, latencies, current_app_allocation, predecessors):
+    num_nodes = latencies.shape[0]
+    pred_allocated_indicator = np.zeros(num_nodes, dtype=np.float32)
+    # has_last_request = np.zeros(num_nodes, dtype=np.float32)
+    for task_idx in predecessors:
+        assigned_node = current_app_allocation[task_idx]
+        if assigned_node >= 0:  # Check if it has been allocated
+            pred_allocated_indicator[assigned_node] = 1.0
+    node_features = np.stack([avg_capacities, min_capacities, node_costs, device_coef, pred_allocated_indicator], axis=1)
     node_features = torch.tensor(node_features, dtype=torch.float32)
 
     edge_index = []

test_model.py
@@ -17,12 +17,12 @@ def test_model(avg_tasks=None, arrival_rate=None, agents=2):
     # np.random.seed(1)
     num_agents = agents
     num_agents_trained = 3
-    env_name = f"CNA_Environment_2_agents_large_workload_2"
+    env_name = f"Environment_4_agents_modified"
     
-    max_ep_len = 2000           # max timesteps in one episode
+    max_ep_len = 4000           # max timesteps in one episode
 
 
-    total_test_episodes = 20    # total num of testing episodes
+    total_test_episodes = 10    # total num of testing episodes
 
     K_epochs = 80               # update policy for K epochs
     eps_clip = 0.2              # clip parameter for PPO
@@ -38,20 +38,19 @@ def test_model(avg_tasks=None, arrival_rate=None, agents=2):
     params = {
         "time_periods": 11,
         "agents": num_agents,
-        "num_nodes_domain": 10,
+        "num_nodes_domain": 5,
         "num_nodes_shared": 10,
-        "capacity_range_domain": (20, 30),
+        "capacity_range_domain": (16, 24),
         "capacity_range_shared": (100, 200),
-        # "num_microservices": 10,
-        "arrival_rate": 5,
+        "arrival_rate": 6,
         "look_ahead_window": 500,
-        # "max_ms": 5,
         "window": 20,
         "max_tasks": 60,
         "task_features": 2,
         "max_dependencies": 100,
         "from_trace": True
     }
+
     if arrival_rate is not None:
         params["arrival_rate"] = arrival_rate
     
@@ -69,7 +68,7 @@ def test_model(avg_tasks=None, arrival_rate=None, agents=2):
     ctde = True
     
     # initialize a PPO agent
-    ppo_agent = PPO_MARL(params['num_nodes_domain'], params['num_nodes_shared'], action_dim, lr_gnn, gamma, K_epochs, eps_clip, params['agents'], inference=True)
+    ppo_agent = PPO_MARL(params['num_nodes_domain'], params['num_nodes_shared'], action_dim, lr_gnn, gamma, K_epochs, eps_clip, params['agents'], inference=True, messages=False, modified=True)
     # preTrained weights directory
 
     random_seed = 0             #### set this to load a particular checkpoint trained on random seed

utils.py
@@ -46,7 +46,7 @@ def create_adj_matrix(num_of_nodes, edge_nodes, fog_nodes, cloud_adj_matrix): #c
                 matrix[i][j] = np.random.randint(1,10)
                 matrix[j][i] = matrix[i][j]
         for j in range(edge[1], fog[1]):
-            matrix[i][j] = np.random.randint(15,25)
+            matrix[i][j] = np.random.randint(10,50)
             matrix[j][i] = matrix[i][j]
             
     for i in range(fog[0], fog[1]):
@@ -58,8 +58,9 @@ def create_adj_matrix(num_of_nodes, edge_nodes, fog_nodes, cloud_adj_matrix): #c
                 matrix[i][j] = cloud_adj_matrix[i-fog[0]][j-fog[0]]
                 matrix[j][i] = matrix[i][j]                        
 
-    adj_matrix = floyd_warshall(matrix)
-    return adj_matrix
+    # adj_matrix = floyd_warshall(matrix)
+    # return adj_matrix
+    return matrix
 
 def reward_func(node_percentage, latency, cost, weights):
     reward = weights['num_of_fragments']*node_percentage - weights['cost']*cost - weights['latency']*latency